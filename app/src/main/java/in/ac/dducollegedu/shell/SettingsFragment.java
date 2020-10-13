88666740fe64532f8bccdf977f7dfc8df7f170a9
abhinandanarya06 <abhinandanarya06122000@gmail.com>
2020-10-13 17:31:01 +0530

    Settings Option added

---
+++ app/src/main/java/in/ac/dducollegedu/shell/SettingsFragment.java
@@ -0,0 +1,13 @@
+package in.ac.dducollegedu.shell;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    }
+}

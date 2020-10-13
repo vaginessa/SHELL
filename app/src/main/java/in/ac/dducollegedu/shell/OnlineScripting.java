2c48f3d57c0ebc8141099854aec50462ff196d17
abhinandanarya06 <abhinandanarya06122000@gmail.com>
2020-10-13 23:25:16 +0530

    Online Scripting tab added, settings modified, added to navigation

---
+++ app/src/main/java/in/ac/dducollegedu/shell/OnlineScripting.java
@@ -0,0 +1,30 @@
+package in.ac.dducollegedu.shell;
+
+import android.os.Bundle;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import in.ac.dducollegedu.shell.databinding.FragmentOnlineScriptingBinding;
+
+
+public class OnlineScripting extends Fragment {
+    FragmentOnlineScriptingBinding binding;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_online_scripting, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onStart() {
+        binding.onlineScriptingWeb.loadUrl("https://rextester.com/l/bash_online_compiler");
+        binding.onlineScriptingWeb.getSettings().setJavaScriptEnabled(true);
+        super.onStart();
+    }
+}
